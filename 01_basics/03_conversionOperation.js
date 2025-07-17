cc537abd943e062bf3c2c85c0045a7945625e67c
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-07-17 08:04:00 +0530

    Create 03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "63abc"
+
+console.log(typeof score);
+// or
+console.log(typeof (score));
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0
+
+let isLoggedIN = 1
+
+let boolIsLoggedIn = Boolean(isLoggedIN)
+console.log(boolIsLoggedIN);
+
+// 1 => true; 0 => false
+// "" => false; "ddfds" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
