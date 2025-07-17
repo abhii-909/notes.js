e3c0cd1e595042d280081046157b4b065324f502
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-07-17 08:23:59 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "63abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 // or
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNum = Number(score)
-console.log(typeof valueInNum);
-console.log(valueInNum);
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
 
 // "33" = 33
 // "33abc" = NaN
@@ -15,7 +15,7 @@ console.log(valueInNum);
 let isLoggedIN = 1
 
 let boolIsLoggedIn = Boolean(isLoggedIN)
-console.log(boolIsLoggedIN);
+// console.log(boolIsLoggedIN);
 
 // 1 => true; 0 => false
 // "" => false; "ddfds" = true
@@ -23,6 +23,39 @@ console.log(boolIsLoggedIN);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************** OPERATIONS ****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power
+// console.log(2/3);
+// console.log(2%3); // remainder
+
+let str1 = "hello"
+let str2 = " abhii"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+// but this should never be used
+
+let counter = 100
+counter++ // postfix
+++counter //prefix
+console.log(counter);
 
