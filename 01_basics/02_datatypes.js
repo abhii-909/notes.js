b63517aeccad702a7f3dd091c65abf2592b7a851
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-07-17 07:49:43 +0530

    Create 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS codeas newer version
+
+// alert(3 + 3);  // won't work cuz we're using nodeJS and not browser
+
+let name  = "Abhii"
+let age = 34
+let isLoggedIN = true
+let state = null
+
+// number => 2 ^ 52
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
